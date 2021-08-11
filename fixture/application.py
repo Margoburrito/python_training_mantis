dcfbad8e5733db06d5b73083e1c023a7b055fcc6
Margoburrito <rchistyakova@gmail.com>
2021-08-11 12:32:32 +0300

    урок 9

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.projecthelper import ProjectHelper
-
+from fixture.james import JamesHelper
+from fixture.mail import MailHelper
+from fixture.signup import SignupHelper
+from fixture.soap import SoapHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -16,8 +19,13 @@ class Application:
             raise ValueError("Unrecognized browser %s" % browser)
         #self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.james = JamesHelper(self)
         self.projecthelper = ProjectHelper(self)
-        self.base_url = base_url
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+        self.soap = SoapHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def open_home_page(self):
         wd = self.wd
